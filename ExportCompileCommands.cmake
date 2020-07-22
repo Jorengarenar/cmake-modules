ae557fe03d615337dd5d278e0c835de5b7250195
Jorengarenar <jorengarenar@protonmail.com>
2020-07-22 04:06:48 +0200

    Relative path to build/

--- ExportCompileCommands.cmake
+++ ExportCompileCommands.cmake
@@ -1,7 +1,11 @@
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 if(NOT EXISTS "${CMAKE_SOURCE_DIR}/compile_commands.json")
-  execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
-    "${CMAKE_BINARY_DIR}/compile_commands.json"
-    "${CMAKE_SOURCE_DIR}/compile_commands.json")
+  file(RELATIVE_PATH buildRelPath "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}")
+
+  execute_process(
+    COMMAND
+      ${CMAKE_COMMAND} -E create_symlink
+      "${buildRelPath}/compile_commands.json"
+      "${CMAKE_SOURCE_DIR}/compile_commands.json")
 endif()
