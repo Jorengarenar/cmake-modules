fc560b18740c868f40f5706e6635862c9ee0ce06
Jorengarenar <dev@joren.ga>
2020-12-12 14:53:26 +0100

    Add variables

--- PreventInSourceBuilds.cmake
+++ PreventInSourceBuilds.cmake
@@ -1,6 +1,9 @@
 # Author:  Jorengarenar <https://joren.ga>
 # License: MIT
 
+set(CMAKE_DISABLE_SOURCE_CHANGES ON)
+set(CMAKE_DISABLE_IN_SOURCE_BUILD ON)
+
 if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
   message(
     FATAL_ERROR
